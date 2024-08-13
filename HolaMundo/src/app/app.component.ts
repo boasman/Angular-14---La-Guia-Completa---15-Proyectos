76af91356a58d84d91decb62942e0f7147676aff
Elvis Boasman <Elvisboasman@gmail.com>
2024-08-13 15:50:17 +0200

    ok

--- HolaMundo/src/app/app.component.ts
+++ HolaMundo/src/app/app.component.ts
@@ -9,5 +9,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'HolaMundo';
+
+  texto = 'Esto es un video sobre event binding';
+  // nombre = 'Elvis';
+  // textoPlaceHolder = 'Escriba algo aqui';
+  // deshabilitado = true;
+  // imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
+
+  // constructor(){
+  //   setInterval(() => this.deshabilitado = false, 3000);
+  // }
+
+  // getSuma(numero1: number,  numero2: number){
+  //   return numero1 + numero2;
+  // }
+
+  cambiarTexto(): void{
+    this.texto = 'En el proximo video vamos a ver Two way data-binding';
+  }
 }
