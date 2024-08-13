84886e1226f181f27d9b908752e19d490d297d52
Elvis Boasman <Elvisboasman@gmail.com>
2024-08-13 16:57:18 +0200

    DataBindign

--- HolaMundo/src/app/app.component.ts
+++ HolaMundo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,13 +14,13 @@ export class AppComponent {
   title = "Hola Mundo";
   texto = 'Esto es un video sobre event binding';
   // nombre = 'Elvis';
-  // textoPlaceHolder = 'Escriba algo aqui';
-  // deshabilitado = true;
-  // imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
+  textoPlaceHolder = 'Escriba algo aqui';
+  deshabilitado = true;
+  imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
 
-  // constructor(){
-  //   setInterval(() => this.deshabilitado = false, 3000);
-  // }
+  constructor(){
+    setInterval(() => this.deshabilitado = false, 3000);
+  }
 
   // getSuma(numero1: number,  numero2: number){
   //   return numero1 + numero2;
