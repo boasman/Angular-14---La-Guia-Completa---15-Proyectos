10c2179c7dda3a6e5abc59ecb49a96e2eca02916
Elvis Boasman <Elvisboasman@gmail.com>
2024-08-13 22:32:04 +0200

    ver la parte de swith

--- HolaMundo/src/app/app.component.ts
+++ HolaMundo/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,28 +6,44 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   title = "Hola Mundo";
-  texto = 'Esto es un video sobre event binding';
-  // nombre = 'Elvis';
-  textoPlaceHolder = 'Escriba algo aqui';
-  deshabilitado = true;
-  imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
+  // texto = 'Esto es un video sobre event binding';
+  // // nombre = 'Elvis';
+  // textoPlaceHolder = 'Escriba algo aqui';
+  // deshabilitado = true;
+  // imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
 
-  constructor(){
-    setInterval(() => this.deshabilitado = false, 3000);
-  }
+  // constructor(){
+  //   setInterval(() => this.deshabilitado = false, 3000);
+  // }
 
   // getSuma(numero1: number,  numero2: number){
   //   return numero1 + numero2;
   // }
 
-  cambiarTexto(): void{
-    this.texto = 'En el proximo video vamos a ver Two way data-binding';
+  // cambiarTexto(): void{
+  //   this.texto = 'En el proximo video vamos a ver Two way data-binding';
+  // }
+
+  listaEstudiantes: any[] = [
+
+    {nombre: "Tomas gonzales", estado: "Promocionado"},
+    {nombre: "Lucas Perez", estado: "Regular"},
+    {nombre: "Juan Garcia", estado: "Regular"},
+    {nombre: "Martha Perez", estado: "Regular"}
+  ]
+
+  mostrar = false;
+
+  toogle():void{
+
+    this.mostrar = !this.mostrar;
+
   }
 }
