f28b3a2800c07a9619db57d75eff35b197d5f1c0
elvis michael paulino <epaulino@rauroszm.com>
2024-09-02 11:32:21 +0200

    Se agrego el metodo para navegar hacia el otro componente al presionar el boton

--- AppBMICalculator/src/app/components/inicio/inicio.component.ts
+++ AppBMICalculator/src/app/components/inicio/inicio.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+edad = 25;
+peso = 60;
+altura = 170;
+sexo = 'Masculino';
 
+constructor(private router: Router){}
+
+cambiarAltura(event: any){
+  this.altura = event.target.value;
+}
+masculino(){
+  this.sexo = 'Masculino';
+}
+femenino(){
+  this.sexo = 'Femenino';
+}
+
+carcularBMI(){
+  // if(this.sexo == 'Masculino'){
+
+  // }
+  // else{
+
+  // }
+  this.router.navigate(['/resultado'])
+}
 }
