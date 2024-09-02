498b794a1ccf0e5136eb40524ba6438a56eb81dc
elvis michael paulino <epaulino@rauroszm.com>
2024-09-02 12:59:48 +0200

    Se le dieron estilos al html

--- AppBMICalculator/src/app/components/resultado/resultado.component.ts
+++ AppBMICalculator/src/app/components/resultado/resultado.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './resultado.component.html',
   styleUrl: './resultado.component.css'
 })
