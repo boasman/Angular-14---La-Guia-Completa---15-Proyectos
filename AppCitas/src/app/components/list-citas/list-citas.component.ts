714210ff0456bea366d35ef5f7ecbcdd1ccf6b83
elvis michael paulino <epaulino@rauroszm.com>
2024-09-03 13:25:53 +0200

    Se agrego la imagen de fondo de fondo y los gradiente para mostrar mejor la imagen

---
+++ AppCitas/src/app/components/list-citas/list-citas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-citas',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-citas.component.html',
+  styleUrl: './list-citas.component.css'
+})
+export class ListCitasComponent {
+
+}
