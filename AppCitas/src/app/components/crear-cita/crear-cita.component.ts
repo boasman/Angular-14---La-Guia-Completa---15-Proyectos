438582bdd587a717c534171b782304ae1818616c
Elvis Boasman <Elvisboasman@gmail.com>
2024-09-05 17:10:05 +0200

    Seguir trabajando en la parte de crear citas

--- AppCitas/src/app/components/crear-cita/crear-cita.component.ts
+++ AppCitas/src/app/components/crear-cita/crear-cita.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-cita',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './crear-cita.component.html',
   styleUrl: './crear-cita.component.css'
 })
 export class CrearCitaComponent {
 
+  nombre='';
+  fecha='';
+  hora='';
+  sintomas='';
+
+  formularioIncorrecto = false;
+
+  agregarCita(){
+
+    if(this.nombre === '' || this.fecha === '' || this.hora === '' || this.sintomas === ''){
+      this.formularioIncorrecto = true;
+      return;
+    }    
+    this.formularioIncorrecto = false;
+
+    //Creamos objeto para enviarselo al padre
+    const CITA = {
+      nombre: this.nombre,     
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas
+    }
+
+    console.log(CITA);
+  }
 }
